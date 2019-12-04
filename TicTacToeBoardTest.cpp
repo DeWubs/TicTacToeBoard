40b63338da2ddf6ee3b0d158227916bc7b081674
DeWubs <rwprouty55@gmail.com>
2019-12-04 15:16:18 -0800

    added some tests for getPeice

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -40,6 +40,54 @@ TEST(TicTacToeBoardTest, placeSecondPeice){
 	game5.placePiece(2,2);
 	ASSERT_EQ(game5.placePiece(1,2), O);
 }
+
+TEST(TicTacToeBoardTest, placePeiceOnTop){
+	TicTacToeBoard game6;
+	game6.placePiece(2,2);
+	ASSERT_EQ(game6.placePiece(2,2), Invalid);
+}
+
+TEST(TicTacToeBoardTest, PlaceOutRow){
+	TicTacToeBoard game7;
+	ASSERT_EQ(game7.placePiece(4,2), Invalid);
+}
+
+TEST(TicTacToeBoardTest, PlaceOutCol){
+	TicTacToeBoard game8;
+	ASSERT_EQ(game8.placePiece(2,4), Invalid);
+}
+
+TEST(TicTacToeBoardTest, PlaceOutRowandCol){
+	TicTacToeBoard game9;
+	ASSERT_EQ(game9.placePiece(4,4), Invalid);
+}
+
+TEST(TicTacToeBoardTest, getNormalPiece){
+	TicTacToeBoard game10;
+	game10.placePiece(1,1);
+	ASSERT_EQ(game10.getPiece(1,1), X);
+}
+
+TEST(TicTacToeBoardTest, getBlank){
+	TicTacToeBoard game11;
+	ASSERT_EQ(game11.getPiece(1,1), Blank);
+}
+
+TEST(TicTacToeBoardTest, getoutofboundsRow){
+	TicTacToeBoard game12;
+	ASSERT_EQ(game12.getPiece(4,1), Invalid);
+}
+
+TEST(TicTacToeBoardTest, getoutofboundsCol){
+	TicTacToeBoard game13;
+	ASSERT_EQ(game13.getPiece(1,4), Invalid);
+}
+
+TEST(TicTacToeBoardTest, getoutofboundsRowandCol){
+	TicTacToeBoard game14;
+	ASSERT_EQ(game14.getPiece(4,4), Invalid);
+}
+
 /* EXAMPLE TEST FORMAT
 TEST(TicTacToeBoardTest, unitTestName)
 {
