ae5daad2e4d76fe7cb0e979a3ca5a1d3ca716bdf
Robert <rwprouty55@gmail.com>
2019-12-04 21:41:04 -0800

    fixed some implementation errors

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -82,29 +82,45 @@ Piece TicTacToeBoard::getPiece(int row, int column)
 Piece TicTacToeBoard::getWinner()
 {
   //check top row
-  if(board[0][0] == board[0][1] && board[0][1] == board[0][2])
-    return board[0][0];
+  if(board[0][0] == board[0][1] && board[0][1] == board[0][2]){
+    if(board[0][0] != Blank)
+      return board[0][0];
+  }
   //check middle row
-  else if(board[1][0] == board[1][1] && board[1][1] == board[1][2])
-    return board[2][0];
+  else if(board[1][0] == board[1][1] && board[1][1] == board[1][2]){
+    if(board[1][0] != Blank)
+      return board[1][0];
+  }
   //check bottom row
-  else if(board[2][0] == board[2][1] && board[2][1] == board[2][2])
-    return board[2][0];
+  else if(board[2][0] == board[2][1] && board[2][1] == board[2][2]){
+    if(board[2][0] != Blank)
+      return board[2][0];
+  }
   //check left vertical
-  else if(board[0][0] == board[1][0] && board[1][0] == board[2][0])
-    return board[0][0];
+  else if(board[0][0] == board[1][0] && board[1][0] == board[2][0]){
+    if(board[0][0] != Blank)
+      return board[0][0];
+  }
   //check middle vertical
-  else if(board[0][1] == board[1][1] && board[1][1] == board[2][2])
-    return board[0][1];
+  else if(board[0][1] == board[1][1] && board[1][1] == board[2][2]){
+    if(board[0][1] != Blank)
+      return board[0][1];
+  }
   //check right vertical
-  else if(board[0][2] == board[1][1] && board[1][1] == board[2][2])
-    return board[0][2];
+  else if(board[0][2] == board[1][1] && board[1][1] == board[2][2]){
+    if(board[0][2] != Blank)
+      return board[0][2];
+  }
   //check topLeft diag
-  else if(board[0][0] == board[1][1] && board[1][1] == board[2][2])
-    return board[0][0];
+  else if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+    if(board[0][0] != Blank)
+      return board[0][0];
+  }
   //check top right diag
-  else if(board[0][2] == board[1][1] && board[1][1] == board[2][0])
-    return board[0][2];
+  else if(board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+    if(board[0][2] != Blank)
+      return board[0][2];
+  }
   else{
     for(int i = 0; i < BOARDSIZE; i++){
       for(int j = 0; j < BOARDSIZE; j++){
